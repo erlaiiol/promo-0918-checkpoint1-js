ef43e6ddb281a3771d52353e59ac2fd4d4cff511
Eliott R <eliott.raillere@gmail.com>
2018-10-05 11:04:52 +0200

    Algo3 résolu (avec une fraude à base de random pour remplacer l'input sisi)

--- algo3.js
+++ algo3.js
@@ -2,8 +2,25 @@
 * Given a number N return the index value of the Fibonacci sequence.
 * Ex: fibo(6)  // return 13;
 * Ex: fibo(10) // return 89;
-*/
+There's a mistake: 6th index of fibonacci numbers is 8, not 13; therefore fibonacci 7th index is 13. 
+1, 1, 2, 3, 5, 8, 13, 21*/
+let N=Math.floor(Math.random()*11)//any number between
+console.log("The fibonacci index is: " + N)
 
 function fibonacci(num) {
-  
+    let fibnumber=[1];
+
+    for (let i=0; i<N+1; i++){
+        if (fibnumber[i-1]==undefined || fibnumber[i-2]==undefined){
+            fibnumber[i]=i
+        }
+        else {
+            fibnumber[i]=fibnumber[i-2]+fibnumber[i-1]
+        }
+        
+    }
+    console.log(fibnumber[N]);
 }
+
+
+fibonacci(N)
