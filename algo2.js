4e3213eaf1cac05b58f41236bc9dd318fd88ce17
Eliott R <eliott.raillere@gmail.com>
2018-10-05 11:59:17 +0200

    Resolution algo2.js fin du boulot zer

--- algo2.js
+++ algo2.js
@@ -1,34 +1,32 @@
-/* 
-* Corriger la fonction minMax()
-* La fonction récupère un tableau de nombres positifs, négatifs ou nuls
-* Et retourne un tableau à 2 entrées contenant la valeur minimale
-* et la valeur maximale du tableau récupéré en entrée
-* 
-* Exemple 1 :
-* Tableau d'entrée : [4, 6, 35, -65, -9, 0, 67]
-* Résultat : [-65, 67]
-* 
-* Exemple 2 :
-* Tableau d'entrée : [-30, 5, 43, 108, -5, -7, 89]
-* Résultat : [-30, 108]
-* 
-* Exemple 3 :
-* Tableau d'entrée : [56, 7, 63, 9, 7, 12, 85]
-* Résultat : [7, 85]
-*/
-
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
-
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
+ 
+//Corriger la fonction minMax()
+//La fonction récupère un tableau de nombres positifs, négatifs ou nuls
+//Et retourne un tableau à 2 entrées contenant la valeur minimale
+//et la valeur maximale du tableau récupéré en entrée
+//
+// Exemple 1 :
+const test = [4, 6, 35, -65, -9, 0, 67]
+// Résultat : [-65, 67]
+// 
+// Exemple 2 :
+// Tableau d'entrée : [-30, 5, 43, 108, -5, -7, 89]
+// Résultat : [-30, 108]
+// 
+// Exemple 3 :
+// Tableau d'entrée : [56, 7, 63, 9, 7, 12, 85]
+// Résultat : [7, 85]
+tableau=[2,1,10,9]
+function minMax(array){
+  leMin=Number.POSITIVE_INFINITY;
+  leMax=Number.NEGATIVE_INFINITY;
+  for (i=0; i<test.length; i++)
+    if (leMin>test[i]){
+      leMin=test[i];
     }
-    if (array[i] = max) {
-      max = array[i];
+    else if (leMax<test[i]){
+      leMax=test[i];
     }
-    return array;
-  }
-  array.push(min, max);
+    console.log("dans le tableau [" + test + "] le minimum est " + leMin + " et le maximum " + leMax)
 }
+
+minMax(test)
